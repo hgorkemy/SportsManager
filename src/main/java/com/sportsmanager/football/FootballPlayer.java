e95e82046f9c15a6bb70dfaa4172e0d9ed8c1d42
Berke Ikikarakayali <berke.karakaya35@outlook.com>
2026-05-06 01:19:18 +0300

    fix: conductTraining method

--- src/main/java/com/sportsmanager/football/FootballPlayer.java
+++ src/main/java/com/sportsmanager/football/FootballPlayer.java
@@ -13,55 +13,44 @@ import java.util.Map;
  */
 public class FootballPlayer extends Player {
 
-    private int speed;
-    private int shooting;
-    private int passing;
-    private int ballControl;
-    private int defending;
-    private int physicality;
+    private final Map<String, Integer> attributes;
 
     public FootballPlayer(String firstName, String lastName, int age,
                           FootballPosition position,
                           int speed, int shooting, int passing,
                           int ballControl, int defending, int physicality) {
         super(firstName, lastName, age, position);
-        this.speed = speed;
-        this.shooting = shooting;
-        this.passing = passing;
-        this.ballControl = ballControl;
-        this.defending = defending;
-        this.physicality = physicality;
+        this.attributes = new HashMap<>();
+        this.attributes.put("speed", speed);
+        this.attributes.put("shooting", shooting);
+        this.attributes.put("passing", passing);
+        this.attributes.put("ballControl", ballControl);
+        this.attributes.put("defending", defending);
+        this.attributes.put("physicality", physicality);
     }
 
     @Override
     public Map<String, Integer> getAttributes() {
-        Map<String, Integer> attrs = new HashMap<>();
-        attrs.put("speed", speed);
-        attrs.put("shooting", shooting);
-        attrs.put("passing", passing);
-        attrs.put("ballControl", ballControl);
-        attrs.put("defending", defending);
-        attrs.put("physicality", physicality);
-        return attrs;
+        return attributes;
     }
 
     @Override
     public int getOverallRating() {
         if (getPosition() == FootballPosition.GOALKEEPER) {
-            return (int) Math.round(defending * 0.50 + physicality * 0.20 + speed * 0.15 + passing * 0.10 + ballControl * 0.05);
+            return (int) Math.round(getDefending() * 0.50 + getPhysicality() * 0.20 + getSpeed() * 0.15 + getPassing() * 0.10 + getBallControl() * 0.05);
         } else if (getPosition() == FootballPosition.DEFENDER) {
-            return (int) Math.round(defending * 0.40 + physicality * 0.20 + speed * 0.20 + passing * 0.10 + ballControl * 0.10);
+            return (int) Math.round(getDefending() * 0.40 + getPhysicality() * 0.20 + getSpeed() * 0.20 + getPassing() * 0.10 + getBallControl() * 0.10);
         } else if (getPosition() == FootballPosition.MIDFIELDER) {
-            return (int) Math.round(passing * 0.30 + ballControl * 0.25 + speed * 0.20 + physicality * 0.15 + shooting * 0.10);
+            return (int) Math.round(getPassing() * 0.30 + getBallControl() * 0.25 + getSpeed() * 0.20 + getPhysicality() * 0.15 + getShooting() * 0.10);
         } else { // FORWARD
-            return (int) Math.round(shooting * 0.40 + speed * 0.25 + passing * 0.15 + ballControl * 0.15 + physicality * 0.05);
+            return (int) Math.round(getShooting() * 0.40 + getSpeed() * 0.25 + getPassing() * 0.15 + getBallControl() * 0.15 + getPhysicality() * 0.05);
         }
     }
 
-    public int getSpeed()       { return speed; }
-    public int getShooting()    { return shooting; }
-    public int getPassing()     { return passing; }
-    public int getBallControl() { return ballControl; }
-    public int getDefending()   { return defending; }
-    public int getPhysicality() { return physicality; }
+    public int getSpeed()       { return attributes.get("speed"); }
+    public int getShooting()    { return attributes.get("shooting"); }
+    public int getPassing()     { return attributes.get("passing"); }
+    public int getBallControl() { return attributes.get("ballControl"); }
+    public int getDefending()   { return attributes.get("defending"); }
+    public int getPhysicality() { return attributes.get("physicality"); }
 }
--- src/main/java/com/sportsmanager/handball/HandballPlayer.java
+++ src/main/java/com/sportsmanager/handball/HandballPlayer.java
@@ -11,36 +11,25 @@ import java.util.Map;
  */
 public class HandballPlayer extends Player {
 
-    private int throwing;
-    private int speed;
-    private int agility;
-    private int jumping;
-    private int defending;
-    private int stamina;
+    private final Map<String, Integer> attributes;
 
     public HandballPlayer(String firstName, String lastName, int age,
                           HandballPosition position,
                           int throwing, int speed, int agility,
                           int jumping, int defending, int stamina) {
         super(firstName, lastName, age, position);
-        this.throwing  = throwing;
-        this.speed     = speed;
-        this.agility   = agility;
-        this.jumping   = jumping;
-        this.defending = defending;
-        this.stamina   = stamina;
+        this.attributes = new HashMap<>();
+        this.attributes.put("throwing",  throwing);
+        this.attributes.put("speed",     speed);
+        this.attributes.put("agility",   agility);
+        this.attributes.put("jumping",   jumping);
+        this.attributes.put("defending", defending);
+        this.attributes.put("stamina",   stamina);
     }
 
     @Override
     public Map<String, Integer> getAttributes() {
-        Map<String, Integer> attrs = new HashMap<>();
-        attrs.put("throwing",  throwing);
-        attrs.put("speed",     speed);
-        attrs.put("agility",   agility);
-        attrs.put("jumping",   jumping);
-        attrs.put("defending", defending);
-        attrs.put("stamina",   stamina);
-        return attrs;
+        return attributes;
     }
 
     @Override
@@ -48,20 +37,20 @@ public class HandballPlayer extends Player {
         HandballPosition pos = (HandballPosition) getPosition();
         return switch (pos) {
             case GOALKEEPER -> (int) Math.round(
-                    defending * 0.50 + agility * 0.20 + jumping * 0.15 + stamina * 0.10 + speed * 0.05);
+                    getDefending() * 0.50 + getAgility() * 0.20 + getJumping() * 0.15 + getStamina() * 0.10 + getSpeed() * 0.05);
             case WING       -> (int) Math.round(
-                    speed    * 0.35 + throwing * 0.35 + agility * 0.15 + stamina * 0.10 + defending * 0.05);
+                    getSpeed()    * 0.35 + getThrowing() * 0.35 + getAgility() * 0.15 + getStamina() * 0.10 + getDefending() * 0.05);
             case BACK       -> (int) Math.round(
-                    throwing * 0.35 + jumping  * 0.30 + speed   * 0.15 + agility * 0.10 + stamina   * 0.10);
+                    getThrowing() * 0.35 + getJumping()  * 0.30 + getSpeed()   * 0.15 + getAgility() * 0.10 + getStamina()   * 0.10);
             case PIVOT      -> (int) Math.round(
-                    defending * 0.35 + jumping * 0.30 + stamina * 0.20 + throwing * 0.10 + agility  * 0.05);
+                    getDefending() * 0.35 + getJumping() * 0.30 + getStamina() * 0.20 + getThrowing() * 0.10 + getAgility()  * 0.05);
         };
     }
 
-    public int getThrowing()  { return throwing; }
-    public int getSpeed()     { return speed; }
-    public int getAgility()   { return agility; }
-    public int getJumping()   { return jumping; }
-    public int getDefending() { return defending; }
-    public int getStamina()   { return stamina; }
+    public int getThrowing()  { return attributes.get("throwing"); }
+    public int getSpeed()     { return attributes.get("speed"); }
+    public int getAgility()   { return attributes.get("agility"); }
+    public int getJumping()   { return attributes.get("jumping"); }
+    public int getDefending() { return attributes.get("defending"); }
+    public int getStamina()   { return attributes.get("stamina"); }
 }
